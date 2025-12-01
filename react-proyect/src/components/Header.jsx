87788779b75bfc36ca00ad9f3746cf18acc23662
Izani <iachega06github@gmail.com>
2025-12-01 14:43:05 +0100

    avanzando

--- react-proyect/src/components/Header.jsx
+++ react-proyect/src/components/Header.jsx
@@ -1,18 +1,19 @@
-// import logo from '../assets/logo.png' --- IGNORE ---
+import logo from "../assets/logo.png";
+import "./Header.css";
 
-
-function Header() {
+export default function Header() {
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
+    <nav className="custom-header navbar navbar-expand-lg shadow-sm p-3">
       <div className="container">
+
         {/* LOGO */}
-        <a className="navbar-brand d-flex align-items-center" href="/">
-            <img 
-              src={logo} 
-              alt="Logo" 
-              style={{ height: "50px" }} 
-              className="me-2"
-            />
+        <a className="navbar-brand d-flex align-items-center" href="#">
+          <img
+            src={logo}
+            alt="Logo"
+            style={{ height: "60px" }}
+            className="me-3"
+          />
         </a>
 
         {/* BOTÓN MÓVIL */}
@@ -21,6 +22,9 @@ function Header() {
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -37,11 +41,13 @@ function Header() {
             <li className="nav-item">
               <a className="nav-link" href="/contacto">Contacto</a>
             </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/DondeEncontrarnos">Donde Encontrarnos</a>
+            </li>
           </ul>
         </div>
+
       </div>
     </nav>
   );
 }
-
-export default Header;
